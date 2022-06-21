7afbc05aa9b36cc411405ebd77a1739dc1b0b54b
Adit0507 <adi05072002@gmail.com>
2022-06-22 02:03:25 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
+import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
+import {
+  Ecommerce,
+  Orders,
+  Calendar,
+  Employees,
+  Stacked,
+  Pyramid,
+  Customers,
+  Kanban,
+  Line,
+  Area,
+  Bar,
+  Pie,
+  Financial,
+  ColorPicker,
+  ColorMapping,
+  Editor,
+} from "./pages";
+
 import "./App.css";
 
 const App = () => {
@@ -55,7 +75,7 @@ const App = () => {
               <Route path="/editor" element="Editor" />
               <Route path="/calendar" element="Calendar" />
               <Route path="/color-picker" element="Color Picker" />
- 
+
               {/* Charts */}
               <Route path="/line" element="Line" />
               <Route path="/area" element="Area" />
@@ -65,8 +85,6 @@ const App = () => {
               <Route path="/color-mapping" element="ColorMapping" />
               <Route path="/pyramid" element="Pyramid" />
               <Route path="/stacked" element="Stacked" />
-
-
             </Routes>
           </div>
         </div>

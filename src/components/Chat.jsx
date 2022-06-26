fa9bd455268e360133b383eeb0430274814ac680
Adit0507 <adi05072002@gmail.com>
2022-06-26 18:36:24 +0530

    test

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import {MdOutlineCancel} from 'react-icons/md'
+
+import {Button} from '.';
+import { chatData } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Chat = () => {
+  const { currentColor } = useStateContext();
+
   return (
-    <div>Chat</div>
+    
   )
 }
 
